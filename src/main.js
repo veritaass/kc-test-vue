cb5d685a052c831676f68022aac7a126855596a9
김형태(KIM Hyungtae)/제조Digital플랫폼그룹/SK <07649@skcc.com>
2022-07-05 15:52:06 +0900

    1

--- src/main.js
+++ src/main.js
@@ -69,10 +69,10 @@ else{
   keycloak.init({ 
     onLoad: initOptions.onLoad 
   }).then((auth) => {
-    console.log(auth)
+    console.log(keycloak.idTokenParsed.test_app_groups)
     router.beforeEach((to, from, next)=>{
       const { authorization } = to.meta;
-      if(authorization.length && !authorization.includes(auth))
+      if(authorization.length && !authorization.includes(keycloak.idTokenParsed.test_app_groups))
         return next({ path: "/not-found" })
     })
 
